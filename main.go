57850620afead212a196700338889f753925117f
WalidOEa <welamriti57@gmail.com>
2026-03-25 15:39:59 +0000

    Create main.go

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"sync"
+)
+
+var (
+	count int
+	mu    sync.Mutex
+	port  = 5050
+)
+
+func tallyHandle(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	curr := count
+	mu.Unlock()
+
+	slog.Info("Tally incremented",
+		"curr_count", curr,
+		"remote_addr", r.RemoteAddr,
+	)
+
+	fmt.Fprintf(w, "%d", curr)
+}
+
+func main() {
+	http.HandleFunc("/tally", tallyHandle)
+	slog.Info("Server-",
+		"port", port,
+		"status", "ready",
+	)
+
+	err := (http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
